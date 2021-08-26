33ff42fe7345c113921ad6203edc1d9cd428fb54
Sean <sdillon215@gmail.com>
2021-08-26 16:48:34 -0700

    database calls

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// connect to db
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // defualt response for any other request (Not Found) *must be last*
 app.use((req, res) => {
